83eaa017dcdfa2040a60bde7dde0ad29d20dc431
dima <naumenko.dmitriy@gmail.com>
2021-10-16 17:34:53 -0700

    insert temporary clipboard logic using scripts

--- src/utils.cpp
+++ src/utils.cpp
@@ -4,7 +4,47 @@
 
 #include "utils.h"
 
-void ToClipboard(std::string)
+void ToClipboard(std::string pass)
 {
-    
+    //going to cheat and use cli piping for now.
+    //TODO: Actual API clipboard integration
+    int result;
+    std::string cmd;
+    std::string cleanPass;
+
+    cleanPass = CleanString(pass);
+#ifdef _WIN32
+    cmd = "echo " + cleanPass + "| xclip -selection c";
+    result = system(const_cast<char*>(cmd.c_str()));
+#endif
+#ifdef TARGET_OS_MAC
+    cmd = cleanPass + "| pbcopy";
+    result = system(const_cast<char*>(cmd.c_str()));
+#endif
+#ifdef __linux__
+    cmd = "echo -n " + cleanPass + "| xclip -selection c";
+    result = system(const_cast<char*>(cmd.c_str()));
+#endif
+}
+
+std::string CleanString(std::string pass)
+{
+    std::string cleanString = "";
+    for(int i = 0; i < pass.length(); i++)
+    {
+        if((pass[i] >= 33 && pass[i] <= 36) ||
+           (pass[i] >= 38 && pass[i] <= 42) ||
+           (pass[i] >= 59 && pass[i] <= 63) ||
+           (pass[i] >= 91 && pass[i] <= 96) ||
+           (pass[i] >= 123 && pass[i] <= 126))
+        {
+            cleanString = cleanString + "'" + pass[i] + "'";
+        }
+        else
+        {
+            cleanString += pass[i];
+        }
+    }
+
+    return cleanString;
 }
