2956007376f1d704f7b1d7eb49c54542eab51a9d
dima <naumenko.dmitriy@gmail.com>
2021-10-19 17:46:56 -0700

    file I/O and encryption fixes

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,18 @@ int main(int argc, char* argv[])
     std::string pass;
     std::string encpass;
 
+    if((std::string(argv[1])) == "config")
+    {
+        CreateConfigFile();
+        return 0;
+    }
+
+    if(!ConfigFilesExists())
+    {
+        std::cout << "No config file found. Please run with the config argument." << std::endl;
+        return 0;
+    }
+
     if(argc < 2)
     {
         std::cout << "Please provide command line arguments." << std::endl;
@@ -18,14 +30,14 @@ int main(int argc, char* argv[])
     if((std::string(argv[1])) == "new")
     {
         pass = ParseNew(argc, argv);
+
+        std::cout << "Your new password: " << pass << std::endl;
+        ToClipboard(pass);
     }
-
-    std::cout << "Your new password: " << pass << std::endl;
-    ToClipboard(pass);
-
-    encpass = Encrypt(pass, "");
-    std::cout << encpass << std::endl;
-    std::cout << Decrypt(encpass, "") << std::endl;
+    else if((std::string(argv[1])) == "testlogin")
+    {
+        ValidateMaster();
+    }
 
     return 0;
 }
